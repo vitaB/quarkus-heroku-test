99f6900c34c2b95138b3b345a6d9b4b70de1e6ad
Viktor Befort <v.befort@googlemail.com>
2020-07-05 13:51:48 +0200

    set webhook url with options

--- bot/src/main/kotlin/com/webhook/bot/TelegramRoute.kt
+++ bot/src/main/kotlin/com/webhook/bot/TelegramRoute.kt
@@ -1,6 +1,7 @@
 package com.webhook.bot
 
 import org.apache.camel.quarkus.kotlin.routes
+import org.eclipse.microprofile.config.inject.ConfigProperty
 import javax.enterprise.context.ApplicationScoped
 import javax.enterprise.inject.Produces
 
@@ -8,9 +9,12 @@ import javax.enterprise.inject.Produces
 @ApplicationScoped
 class TelegramRoute() {
 
+    @ConfigProperty(name = "webhook.external.url")
+    lateinit var webhookUrl: String
+
     @Produces
     fun myRoutes() = routes {
-        from("webhook:telegram:bots")
+        from("webhook:telegram:bots?webhookExternalUrl=$webhookUrl")
                 .log("\${body}")
     }
 }
